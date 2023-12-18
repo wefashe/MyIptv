86b40823d6ee30d4f464eca30a62d5af6268f6c3
wefashe <wefashe@qq.com>
2023-12-18 21:05:14 +0800

    测试

---
+++ js/player.js
@@ -0,0 +1,13 @@
+  var video = document.getElementById('video');
+  var vurl = location.search.split('vurl=')[1];
+  // var videoSrc = vurl;
+  var videoSrc = 'https://0472.org/hls/cctv13.m3u8';
+  // 检查浏览器是否支持hls
+  if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = videoSrc;
+  // 如不支持则调用hls.min.js
+  } else if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource(videoSrc);
+    hls.attachMedia(video);
+  }

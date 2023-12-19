b3b2cc6f912b96748a419080e80efcc858fd0b6e
wefashe <wefashe@qq.com>
2023-12-19 19:25:37 +0800

    添加播放列表

--- js/player.js
+++ js/player.js
@@ -1,13 +1,34 @@
-  var video = document.getElementById('video');
-  var vurl = location.search.split('vurl=')[1];
-  // var videoSrc = vurl;
-  var videoSrc = 'https://0472.org/hls/cctv13.m3u8';
-  // 检查浏览器是否支持hls
-  if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    video.src = videoSrc;
-  // 如不支持则调用hls.min.js
-  } else if (Hls.isSupported()) {
-    var hls = new Hls();
-    hls.loadSource(videoSrc);
-    hls.attachMedia(video);
-  }
+var ul =  document.getElementById("video-list");
+var url = location.search.split('url=')[1];
+if(!url){
+	var li = ul.querySelector('li');
+	url = li.getAttribute('value');
+}
+changeVideo(url)
+
+var list = ul.querySelectorAll('li');
+for (var i = 0; i < list.length; i++) {
+list[i].onclick =  function (){
+	changeVideo(this.getAttribute('value'))
+	history.replaceState(null, null, window.location.href.split('?')[0]);
+}
+}
+
+function changeVideo(videoSrc){
+	video = document.getElementById('video');
+	// 检查浏览器是否支持hls
+	if (video.canPlayType('application/vnd.apple.mpegurl')) {
+		video.src = videoSrc;
+	// 如不支持则调用hls.min.js
+	} else if (Hls.isSupported()) { 
+		if(typeof hls !== 'undefined'){
+			hls.destroy();
+		}
+		hls = new Hls();
+		hls.loadSource(videoSrc);
+		hls.attachMedia(video);		
+		hls.on(Hls.Events.MANIFEST_PARSED, function () {
+			 video.play();
+		});
+	}
+}

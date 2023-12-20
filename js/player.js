261cedd68807acf9a62ca590f00ef12d2efd80a9
wefashe <wefashe@qq.com>
2023-12-20 12:51:02 +0800

    样式

--- js/player.js
+++ js/player.js
@@ -18,77 +18,76 @@ if(suffixName === 'm3u8'){
 }
 
 if(/(m3u|txt)$/i.test(suffixName)){
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url, true);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-			var content = xhr.responseText;
-			if(content){
-				content = content.trim();
+	var ul =  document.getElementById("video-list");
+	ajaxHttpRequestFunc(url, ul, (status, ulElement, content) => {
+		if(!status){
+			return;
+		}
+		if(content){
+			content = content.trim();
+		}
+		if(!content){
+			return;
+		}
+				
+		if(suffixName === 'txt'){
+			items = content.split("\n");
+		}
+		if(suffixName === 'm3u'){
+			items = content.substring(content.indexOf("#EXTINF:")).split("#EXTINF:");
+		}
+		var title = document.title;
+		for (var i = 0; i < items.length; i++) {
+			var item = items[i];
+			if(item){
+				item = item.trim();
 			}
-			if(!content){
-				return;
+			if(!item){
+				continue;
 			}
-					
+			if(!item.indexOf('http')){
+				continue;
+			}
 			if(suffixName === 'txt'){
-				items = content.split("\n");
+				m3u8 = item.substring(item.indexOf(',')+1);
+				name = item.substring(0, item.indexOf(','));
 			}
 			if(suffixName === 'm3u'){
-				items = content.substring(content.indexOf("#EXTINF:")).split("#EXTINF:");
+				m3u8 = item.substring(item.lastIndexOf("\n")+1);
+				logo = getValueByKey(item,'tvg-logo');
+				name = getValueByKey(item,'tvg-name');
+				if(typeof name === 'undefined' || name === 'undefined'){
+					name = item.substring(item.lastIndexOf(",")+1,item.lastIndexOf("\n"));
+				}
 			}
-			var title = document.title;
-			var ul =  document.getElementById("video-list");
-			for (var i = 0; i < items.length; i++) {
-				var item = items[i];
-				if(item){
-					item = item.trim();
+			if(m3u8.substr(0, 4) != "http"){
+				continue;
+			}
+			if(typeof logo === 'undefined' || logo === 'undefined'){
+				logo = "https://cdn.jsdelivr.net/gh/wefashe/MyIptv@main/img/logo.png";
+			}
+			var li = document.createElement("li");
+			li.setAttribute("value", m3u8);
+			li.setAttribute("title", name);
+			li.innerHTML = name;
+			li.addEventListener('click',function(e){
+				videoPlay(this.getAttribute('value'));
+				document.title = this.getAttribute('title') +' | ' + title;	
+			})
+			ulElement.appendChild(li);
+			if(ulElement.hidden){
+				videoPlay(m3u8);
+				document.title = name +' | ' + title;	
+				ulElement.hidden = false;
+			}
+			ajaxHttpRequestFunc(m3u8, li, (status, liElement) => {
+				if(!status){
+					liElement.className = "lapse";
 				}
-				if(!item){
-					continue;
-				}
-				if(!item.indexOf('http')){
-					continue;
-				}
-				if(suffixName === 'txt'){
-					m3u8 = item.substring(item.indexOf(',')+1);
-					name = item.substring(0, item.indexOf(','));
-				}
-				if(suffixName === 'm3u'){
-					m3u8 = item.substring(item.lastIndexOf("\n")+1);
-					logo = getValueByKey(item,'tvg-logo');
-					name = getValueByKey(item,'tvg-name');
-					if(typeof name === 'undefined' || name === 'undefined'){
-						name = item.substring(item.lastIndexOf(",")+1,item.lastIndexOf("\n"));
-					}
-				}
-				if(m3u8.substr(0, 4) != "http"){
-					continue;
-				}
-				if(typeof logo === 'undefined' || logo === 'undefined'){
-					logo = "https://cdn.jsdelivr.net/gh/wefashe/MyIptv@main/img/logo.png";
-				}
-				var li = document.createElement("li");
-				li.setAttribute("value", m3u8);
-				li.setAttribute("title", name);
-				li.innerHTML = name;
-				li.addEventListener('click',function(e){
-					videoPlay(this.getAttribute('value'));
-					document.title = this.getAttribute('title') +' | ' + title;	
-				})
-				ul.appendChild(li);
-				if(ul.hidden){
-					videoPlay(m3u8);
-					document.title = name +' | ' + title;	
-					ul.hidden = false;
-				}
-			}			
+			}, 1000);
 		}
-	};
-	xhr.send();
+	});
 }
-
-
-
 
 function getValueByKey(content, key){
 	var regex = new RegExp(`${key}=".*?"`);
@@ -105,6 +104,51 @@ function getValueByKey(content, key){
 	return content;
 }
 
+function ajaxHttpRequestFunc(URL, element, callback, time = 20000) {
+    // 创建XMLHttpRequest对象，即一个用于保存异步调用对象的变量
+    let xmlHttpRequest;
+    // 老版本的 Internet Explorer（IE5 和 IE6）使用 ActiveX 对象
+    // 所有现代浏览器（Chrom、IE7+、Firefox、Safari 以及 Opera）都有内建的 XMLHttpRequest 对象
+    if (window.ActiveXObject) {
+        xmlHttpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+    } else if (window.XMLHttpRequest) { // Netscape浏览器中的创建方式
+        xmlHttpRequest = new XMLHttpRequest();
+    }
+    // 设置响应http请求状态变化的事件
+    xmlHttpRequest.onreadystatechange = function () {
+        // console.log('请求过程', xmlHttpRequest.readyState);
+        // 判断异步调用是否成功,若成功开始局部更新数据
+        if (xmlHttpRequest.readyState == 4) {
+            // console.log('状态码为', xmlHttpRequest.status);
+            if (xmlHttpRequest.status == 200) {
+                // console.log('异步调用返回的数据为：', xmlHttpRequest.responseText);
+                callback(true, element, xmlHttpRequest.responseText);
+                //此处可以写当前URL有效的一个操作
+            } else { // 如果异步调用未成功,抛出错误,并显示错误状态码
+                //此处可以写当前URL无效的一个操作
+                // throw "error:HTTP状态码为:" + xmlHttpRequest.status
+                callback(false, element);
+                console.error("error:HTTP状态码为:" + xmlHttpRequest.status)
+                // return false
+            }
+        }
+    }
+	// 超时时间，单位是毫秒
+	xmlHttpRequest.timeout = time; 
+	xmlHttpRequest.onload = function () {
+	  // 请求完成。在此进行处理。
+	  console.log('请求完成', URL);
+	};
+	xmlHttpRequest.ontimeout = function (e) {
+	  // XMLHttpRequest 超时。在此做某事。
+	  callback(false, element);
+	  console.log('请求超时', URL);
+	};
+    // 创建http请求，并指定请求得方法（get）、url（需要校验的URL）以及验证信息
+    xmlHttpRequest.open("GET", URL, true);
+    // 发送请求，因为是get的请求，可以不携带参数
+    xmlHttpRequest.send(null);
+}
 
 function videoPlay(videoSrc){
 	video = document.getElementById('video');

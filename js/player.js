e0f25a491a7ae08369fcc812fc51bd1421293070
wefashe <wefashe@qq.com>
2023-12-25 17:49:37 +0800

    添加m3u8链接

--- js/player.js
+++ js/player.js
@@ -98,7 +98,7 @@ function createList(ulElement, content, suffixName){
 				if(!status){
 					liElement.className = "lapse";
 				}
-			}, 1500);
+			}, 5000);
 		}
 }
 
@@ -289,4 +289,46 @@ window.onkeydown = function(){
 			video.currentTime = video.currentTime + 5;
 			break;
     }    
-}
+};
+
+
+(function(open, send) {
+   window.timing = new Map();
+   var xhrOpenRequestUrl;  // capture method and url
+   var xhrSendResponseUrl; // capture request body
+   var responseData;       // captured response
+
+   XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+      xhrOpenRequestUrl = new URL(url, document.location.href).href;
+      window.timing[xhrOpenRequestUrl] = {
+        begin: new Date().valueOf(),
+        method: method
+      }
+
+      open.apply(this, arguments); 
+   };
+
+   XMLHttpRequest.prototype.send = function(data) {
+	   
+       this.setRequestHeader('Origin', 'https://0472.org/');
+	   this.setRequestHeader("referer", "http://www.oslang.cn/");
+       this.addEventListener('readystatechange', function() {
+          var gaPageUrl=document.location.href;
+          var gaTitle=document.title;
+          if (window.timing[this.responseURL] && this.readyState === 4) {
+            xhrSendResponseUrl = this.responseURL;
+            var duration = new Date().valueOf() - window.timing[xhrSendResponseUrl].begin;
+            console.log({
+                  hitType: 'timing',
+                  timingCategory: gaTitle + "-" + document.location.href, 
+                  timingVar: window.timing[xhrSendResponseUrl].method + ':' + xhrSendResponseUrl ,
+                  timingValue: duration,
+                  timingLabel: JSON.stringify(data)
+                })
+          }
+        }, false);
+      
+      send.apply(this, arguments); 
+   }
+
+})(XMLHttpRequest.prototype.open, XMLHttpRequest.prototype.send)
